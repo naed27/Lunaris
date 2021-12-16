3524900e24e0a5ac9997f73fdde477a7b4a1094f
Dean <naed221@gmail.com>
2021-12-16 14:54:53 +0800

    simplified channel for game

---
+++ src/Games/CloverClub/gameChannelManager.ts
@@ -0,0 +1,40 @@
+import { GuildMember, Message, MessageEmbed, TextChannel }  from 'discord.js';
+import { createEmbed } from '../../Helpers/toolbox';
+import ChannelManager from './channelManager';
+import Game from './game';
+import { Phase } from './phases';
+
+interface ConstructorParams{
+  channel:TextChannel,
+  game:Game
+}
+
+interface timerParams{
+  currentPhase:Phase,
+  secondsLeft:number
+}
+
+export default class GameChannelManager extends ChannelManager{
+
+  timerMessageAddress: Message | null = null;
+    
+  constructor({channel,game}: ConstructorParams){
+    
+    super({ 
+      game,
+      channel,
+      defaultId: game.getGameKey().id
+    });
+
+  }
+
+  updateTimer = ({ currentPhase,secondsLeft }: timerParams) =>{
+    if( this.timerMessageAddress === null )return;
+
+    const description = `${currentPhase.name} - ${secondsLeft}`; 
+    const embed = createEmbed({ description });
+    
+    this.timerMessageAddress.edit({ embeds: [embed] });
+  }
+  
+}
