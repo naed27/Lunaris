d61c46b273dea7b688677bfdb3c7f2a69181b757
Dean <naed221@gmail.com>
2022-03-16 17:14:04 +0800

    added error catcher

--- src/Games/Salem/channel/channelManager.ts
+++ src/Games/Salem/channel/channelManager.ts
@@ -31,7 +31,7 @@ export default class ChannelManager{
             SEND_MESSAGES:true,
             VIEW_CHANNEL: true,
             READ_MESSAGE_HISTORY:true 
-        })
+        }).catch(()=>console.log( 'Error: Could not show and unlock channel' ));
     }
     
     hideAndLock = async ( id:string=this.defaultId ) =>{
@@ -39,38 +39,42 @@ export default class ChannelManager{
             SEND_MESSAGES:false,
             VIEW_CHANNEL: false,
             READ_MESSAGE_HISTORY:false 
-        });
+        }).catch(()=> console.log( 'Error: Could not hide and lock channel' ));
     }
 
     unlock = async ( id:string=this.defaultId ) =>{
         return await this.channel.permissionOverwrites.edit(id,{ 
             SEND_MESSAGES:true,
-        });
+        }).catch(()=> console.log( 'Error: Could not unlock channel' ));
     }
 
     lock = async ( id:string=this.defaultId ) =>{
         return await this.channel.permissionOverwrites.edit(id,{ 
             SEND_MESSAGES:false,
-        });
+        }).catch(()=> console.log( 'Error: Could not lock channel' ));
     }
 
     hide = async ( id:string=this.defaultId ) =>{
         return await this.channel.permissionOverwrites.edit(id,{ 
             VIEW_CHANNEL: false,
             READ_MESSAGE_HISTORY:false 
-        });
+        }).catch(()=> console.log( 'Error: Could not hide channel' ));
     }
 
     show = async ( id:string=this.defaultId ) =>{
         return await this.channel.permissionOverwrites.edit(id,{ 
             VIEW_CHANNEL: true,
             READ_MESSAGE_HISTORY:true,
-        });
+        }).catch(()=> console.log( 'Error: Could not show channel' ));
     }
     
     sendString = async (a: string | MessagePayload | MessageOptions) => {
         const linebreak = `‎`;
-        return await this.channel.send(`${linebreak}${a}`);
+        return await this.channel
+        .send(`${linebreak}${a}`)
+        .catch(()=> console.log( 'Error: Could not send string message to discord' ));
     }
-    send = async (a: string | MessagePayload | MessageOptions) => await this.channel.send(a);
+    send = async (a: string | MessagePayload | MessageOptions) => await this.channel
+    .send(a)
+    .catch(()=> console.log( 'Error: Could not send message to discord' ));
 }
