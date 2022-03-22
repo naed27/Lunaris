5012df929dfef3becb651db184813a058262d137
Dean <naed221@gmail.com>
2022-03-22 16:09:19 +0800

    fixed format

--- src/Games/Salem/archive/welcome.ts
+++ src/Games/Salem/archive/welcome.ts
@@ -5,28 +5,17 @@ export default ({game, player}:{game: Game, player: Player}) => {
   return [
     //----------------------------------------
     `**Town Of Salem**
-    \nStatus: ${player.getStatus()}
     \nPlayers who are ready: (${game.getPlayers().filter(p=>p.isReady()).length}/${game.getPlayers().length})
     `,
 
     //----------------------------------------
-    `**Profile**
-    \nName: ${player.getDiscord().user.username}
-    \nUsername: ${player.getUsername()}
-    \nStatus: ${player.getStatus()}
-    \nRole: ${player.getRole().getName()}
-    \nAbilities: ${player.getRole().getAbilities().join("\n")}
-    \nSkill Commands: 
-    \n${player.getRole().getCommands().map((c)=> c.getGuide()).join("\n")}
-    \nHow to Win: ${player.getRole().getGoals().join("\n")}
-    `,
+    `**Profile**\n\nName: ${player.getDiscord().user.username}\nUsername: ${player.getUsername()}\nStatus: ${player.getStatus()}\nRole: ${player.getRole().getName()}\nAbilities: ${player.getRole().getAbilities().join("\n")}\nSkill Commands: \n${player.getRole().getCommands().filter(c=>c.getPermission()==='Role Holder').map((c)=> `-${c.getGuide()}`).join("\n")}\nHow to Win: ${player.getRole().getGoals().join("\n")}`,
 
     //----------------------------------------
     `Type ".changename <new name>" to change your in-game name."`,
 
     //----------------------------------------
     `**Town Of Salem**
-    \nStatus: ${player.getStatus()}
     \nPlayers who are ready: (${game.getPlayers().filter(p=>p.isReady()).length}/${game.getPlayers().length})`,
 
     //----------------------------------------
