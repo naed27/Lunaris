aa467a2329415401eeadc1418b4509c3b9ac715c
Dean <naed221@gmail.com>
2022-03-23 17:36:19 +0800

    fixed format of generator: phaseMenu

--- src/Games/Salem/channel/messageManagers/generators.ts
+++ src/Games/Salem/channel/messageManagers/generators.ts
@@ -35,9 +35,7 @@ export const  judge = ({messageManager}:Params) => {
 export const phaseCommandsMenu = ({messageManager}:Params) => {
   const manager = messageManager;
   const player = manager.getPlayer();
-  const string = `Name: ${player.getUsername()}
-    \nRole: ${player.getRoleName()}
-    \nStatus: ${player.getStatus()}`
+  const string = `Name: ${player.getUsername()}\nRole: ${player.getRoleName()}\nStatus: ${player.getStatus()}`
 
   return createEmbed({ description: string });
 }
