4f5456b9350c05081d35ff72dc835bdbc9a6dd68
Dean <naed221@gmail.com>
2022-03-24 13:20:40 +0800

    cleaned format for welcome

--- src/Games/Salem/archive/welcome.ts
+++ src/Games/Salem/archive/welcome.ts
@@ -9,7 +9,7 @@ export default ({game, player}:{game: Game, player: Player}) => {
     `,
 
     //----------------------------------------
-    `**Profile**\n\nName: ${player.getDiscord().user.username}\nUsername: ${player.getUsername()}\nStatus: ${player.getStatus()}\nRole: ${player.getRole().getName()}\nAbilities: ${player.getRole().getAbilities().join("\n")}\nSkill Commands: \n${player.getRole().getCommands().filter(c=>c.getType()==='Skill Command').map((c)=> `-${c.getGuide()}`).join("\n")}\nHow to Win: ${player.getRole().getGoals().join("\n")}`,
+    `**Profile**\n\nName: ${player.getDiscord().user.username}\nUsername: ${player.getUsername()}\nStatus: ${player.getStatus()}\nRole: ${player.getRole().getName()}\nAbilities: ${player.getRole().getAbilities().join("\n")}\nSkill Commands: ${player.getRole().getCommands().filter(c=>c.getType()==='Skill Command').map((c)=> `${c.getName()}`).join(', ')}\nHow to Win: ${player.getRole().getGoals().join("\n")}`,
 
     //----------------------------------------
     `Type ".changename <new name>" to change your in-game name."`,
